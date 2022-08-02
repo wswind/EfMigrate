e1ec669586a37e0becf6773db2a0c6c71a78322b
Vincent Wang <ws_dev@163.com>
2022-08-02 17:39:41 +0800

    update

---
+++ DefaultDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfMigrate
+{
+    public class DefaultDbContext : DbContext
+    {
+        //public DbSet<> Entities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(@"Server=localhost;Database=efmig;User Id=root;Password=root;");
+        }
+    }
+}
